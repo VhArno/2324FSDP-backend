806c68435c4a804e6fe17b5127ffa0e860e4ed0c
Arno Van Hee <arno.vanhee@student.odisee.be>
2024-05-17 16:51:39 +0200

    accountseeder fix

--- app/database/seeders/AccountSeeder.php
+++ app/database/seeders/AccountSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class AccountSeeder extends Seeder
 {
@@ -12,31 +13,29 @@ class AccountSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create roles
-        $accounts = [
-            [
-                'firstname' => 'Super',
-                'lastname' => 'Admin',
-                'email' => 'admin@odisee.be',
-                'password' => 'Azerty123',
-                'role_id' => '1',
-            ],
-            [
-                'firstname' => 'Arno',
-                'lastname' => 'Van Hee',
-                'email' => 'arnovanhee@gmail.com',
-                'password' => 'Azerty123',
-                'role_id' => '2',
-            ],
-            [
-                'firstname' => 'John',
-                'lastname' => 'Doe',
-                'email' => 'Johndoe@odisee.be',
-                'password' => 'Azerty123',
-                'role_id' => '3',
-            ],
-        ];
+        $superAdminUser = new User();
+        $superAdminUser->firstname = 'Super';
+        $superAdminUser->lastname = 'Admin';
+        $superAdminUser->email = 'admin@odisee.be';
+        $superAdminUser->password = 'Azerty123';
+        $superAdminUser->role_id = 1;
 
-        User::insert($accounts);
+        $adminUser = new User();
+        $adminUser->firstname = 'Arno';
+        $adminUser->lastname = 'Van Hee';
+        $adminUser->email = 'arnovanhee@gmail.com';
+        $adminUser->password = 'Azerty123';
+        $adminUser->role_id = 2;
+
+        $user = new User();
+        $user->firstname = 'John';
+        $user->lastname = 'Doe';
+        $user->email = 'Johndoe@odisee.be';
+        $user->password = 'Azerty123';
+        $user->role_id = 3;
+
+        $superAdminUser->save();
+        $adminUser->save();
+        $user->save();
     }
 }
