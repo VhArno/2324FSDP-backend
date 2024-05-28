852b16386edebdecd4678448ef8fa732900210f4
Arno Van Hee <arno.vanhee@student.odisee.be>
2024-05-28 23:58:26 +0200

    answers ondelete cascade

--- app/database/migrations/2024_04_29_192637_create_answers_table.php
+++ app/database/migrations/2024_04_29_192637_create_answers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->text('answer');
             $table->integer('weight')->nullable();
-            $table->foreignId('question_id')->constrained();
+            $table->foreignId('question_id')->constrained()->onDelete('cascade');
             $table->foreignId('specialisation_id')->constrained();
         });
     }
