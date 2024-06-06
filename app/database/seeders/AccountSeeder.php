a48f1aaeda89561164292cbc07141ed6bd8c4535
Arno Van Hee <arno.vanhee@student.odisee.be>
2024-06-06 22:05:08 +0200

    new account seed

--- app/database/seeders/AccountSeeder.php
+++ app/database/seeders/AccountSeeder.php
@@ -13,6 +13,7 @@ class AccountSeeder extends Seeder
      */
     public function run(): void
     {
+        // Superadmin
         $superAdminUser = new User();
         $superAdminUser->firstname = 'Super';
         $superAdminUser->lastname = 'Admin';
@@ -20,12 +21,28 @@ class AccountSeeder extends Seeder
         $superAdminUser->password = 'Azerty123';
         $superAdminUser->role_id = 1;
 
+        // Admin
         $adminUser = new User();
         $adminUser->firstname = 'Arno';
         $adminUser->lastname = 'Van Hee';
         $adminUser->email = 'arnovanhee@gmail.com';
         $adminUser->password = 'Azerty123';
         $adminUser->role_id = 2;
+
+        // Users
+        $user1 = new User();
+        $user1->firstname = 'Bart';
+        $user1->lastname = 'Delrue';
+        $user1->email = 'bart.delrue@odisee.be';
+        $user1->password = 'Azerty123';
+        $user1->role_id = 3;
+
+        $user2 = new User();
+        $user2->firstname = 'Joris';
+        $user2->lastname = 'Maervoet';
+        $user2->email = 'joris.maervoet@odisee.be';
+        $user2->password = 'Azerty123';
+        $user2->role_id = 3;
 
         $user = new User();
         $user->firstname = 'John';
@@ -36,6 +53,8 @@ class AccountSeeder extends Seeder
 
         $superAdminUser->save();
         $adminUser->save();
+        $user1->save();
+        $user2->save();
         $user->save();
     }
 }
